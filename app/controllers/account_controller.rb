278e2c732978735fcada4df82111ac10d299523f
smriti <smriti.154@gmail.com>
2016-12-16 18:44:12 +0530

    cnflit creatin

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,6 +1,6 @@
 sBbuosdbsjd
 jbslosdsdbjsd
 diohishisdhis
-hello i m the second change in app
+hello i m the second change in app   creating conflit
 hell i m the chnge in the feature branch..............
 vsdjv
